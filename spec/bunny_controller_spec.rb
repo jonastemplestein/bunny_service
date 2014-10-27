9ba422ef2379d58343e2cf0fa03dc2a8c2008fa3
Stephen Best <bestie@gmail.com>
2014-10-27 15:34:16 +0000

    Interface for directly triggering RPC actions

--- spec/bunny_controller_spec.rb
+++ spec/bunny_controller_spec.rb
@@ -7,11 +7,11 @@ class TestController < BunnyService::Controller
   )
 
   def foo
-    { result: "foo got #{params['thing']}" }
+    { result: "foo got #{params.fetch('thing')}" }
   end
 
   def bar
-    { result: "bar got #{params['thing']}" }
+    { result: "bar got #{params.fetch('thing')}" }
   end
 end
 
@@ -25,7 +25,7 @@ RSpec.describe BunnyService::Controller do
   }
 
   describe ".listen" do
-    it "handles messages for the action bindings on the given exchange" do
+    it "starts a server for each action binding on the given exchange" do
       Timeout::timeout(5) do
         pid = fork { TestController.listen(exchange_name: exchange_name) }
         at_exit { Process.kill(9, pid) }
@@ -34,6 +34,34 @@ RSpec.describe BunnyService::Controller do
           to eq "result" => "foo got baz"
         expect(client.call('test.bar', thing: 'qux').body).
           to eq "result" => "bar got qux"
+      end
+    end
+  end
+
+  describe "#handle" do
+    let(:controller) {
+      TestController.new(
+        request: BunnyService::Request.new(
+          params: {
+            "thing" => "some thing",
+          },
+          rabbitmq_delivery_info: {},
+          rabbitmq_properties: {},
+        ),
+        response: nil,
+      )
+    }
+
+    it "translates the action name into a message call via the action bindings" do
+      expect(controller.handle(rpc: "test.foo"))
+        .to eq(result: "foo got some thing")
+    end
+
+    context "when the action is not defined" do
+      it "raises an error" do
+        expect {
+          controller.handle(rpc: "action.does_not_exist")
+        }.to raise_error(BunnyService::Controller::ActionNotDefined)
       end
     end
   end
